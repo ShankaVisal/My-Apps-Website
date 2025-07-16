4003b97c47cafc49407140f5acda0d2ade31c3bb
Shanka Visal Amarasekara <shankavisal@gmail.com>
2025-07-16 19:42:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/CallToActionBanner.tsx
+++ src/components/CallToActionBanner.tsx
@@ -42,7 +42,7 @@ export function CallToActionBanner() {
       scale: 1,
       transition: {
         duration: 0.8,
-        ease: [0.6, 0.01, -0.05, 0.95],
+        ease: [0.6, 0.01, 0.05, 0.95],
       },
     },
   }
