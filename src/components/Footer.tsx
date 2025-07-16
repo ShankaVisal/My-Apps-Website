c756c55c90867d19e231e8cbc78277d21ee05f58
Shanka Visal Amarasekara <shankavisal@gmail.com>
2025-07-16 20:23:16 +0000

    social media and other lcon buttons should be go to right side

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,17 +25,15 @@ export default function Footer() {
 
   return (
     <footer className="bg-muted text-muted-foreground">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <p className="text-center text-sm leading-loose md:text-left">
-            Created by{' '}
-            <Link href="https://www.shankavisal.com" target="_blank" rel="noopener noreferrer" className="font-medium underline underline-offset-4 hover:text-primary">
-              Shanka Visal
-            </Link>
-            . © {new Date().getFullYear()} All Rights Reserved.
-          </p>
-        </div>
-        <div className="flex items-center gap-4 flex-wrap justify-center">
+      <div className="container flex flex-col-reverse items-center gap-8 py-8 md:flex-row md:justify-between md:py-4">
+        <p className="text-center text-sm leading-loose md:text-left">
+          Created by{' '}
+          <Link href="https://www.shankavisal.com" target="_blank" rel="noopener noreferrer" className="font-medium underline underline-offset-4 hover:text-primary">
+            Shanka Visal
+          </Link>
+          . © {new Date().getFullYear()} All Rights Reserved.
+        </p>
+        <div className="flex items-center gap-4">
           {socialLinks.map(({ name, icon: Icon, href }) => (
             <Link key={name} href={href} target="_blank" rel="noopener noreferrer">
               <Icon />
